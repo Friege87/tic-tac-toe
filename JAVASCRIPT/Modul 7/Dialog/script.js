cb9dcfbf87f407cc3d8cf505cdc8f20657a43e18
Friege87 <108327458+Friege87@users.noreply.github.com>
2024-03-19 10:41:49 +0100

    Delete JAVASCRIPT directory

--- JAVASCRIPT/Modul 7/Dialog/script.js
+++
@@ -1,8 +0,0 @@
-function openDialog(text) {
-    document.getElementById('dialog').classList.remove('din-none')
-    document.getElementById('dialog-message').innerHTML = text;
-}
-
-function closeDialog() {
-    document.getElementById('dialog').classList.add('din-none')
-}
--- JAVASCRIPT/Modul 7/Notizbuch/script.js
+++
@@ -1,3 +0,0 @@
-function render() {
-    const canvas = document.getElementById('canvas');
-}
